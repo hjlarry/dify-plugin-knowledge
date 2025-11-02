83bce32bdaa6b6bad344cdf1f24110efc2fcb723
r3-yamauchi <yamauchi@r3it.com>
2025-11-02 17:50:20 +0900

    Modification by r3-yamauchi (#6)
    * r3-yamauchi による改修
    
    * meta version require to be 0.0.1
    
    ---------
    
    Co-authored-by: 非法操作 <hjlarry@163.com>

--- provider/knowledge.py
+++ provider/knowledge.py
@@ -7,8 +7,12 @@ from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
 class KnowledgeProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
-        url = credentials.get("api_uri") + "/datasets"
-        headers = {"Authorization": f"Bearer {credentials.get("api_secret")}"}
+        api_uri = credentials.get("api_uri", "")
+        if not api_uri:
+            raise ToolProviderCredentialValidationError("api_uri is required")
+
+        url = f"{api_uri.rstrip('/')}/datasets"
+        headers = {"Authorization": f'Bearer {credentials.get("api_secret")}'}
         res = httpx.get(url, headers=headers)
         if res.status_code != 200:
             raise ToolProviderCredentialValidationError("Invalid credentials, please check your api_uri and api_secret")
